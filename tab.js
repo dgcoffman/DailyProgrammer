b05cb5fbdb4fd9f8af06e62fe97b8b02b8d21912
Daniel Coffman <dgcoffman@gmail.com>
2017-03-06 03:29:13 -0800

    Tab

---
+++ tab.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+const NOTES = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
+const getNoteForKey = (key, fretNumber) =>
+  NOTES[(NOTES.indexOf(key) + fretNumber) % NOTES.length];
+
+const FRET_NUMBER_PATTERN = /(?:\||-)(\d+)(?=\||-)/g;
+const extractNotesFromLine = line => {
+  const key = line.charAt(0);
+  const notes = [];
+  let fretNumber;
+  while (fretNumber = FRET_NUMBER_PATTERN.exec(line)) {
+    notes.push({
+      name: getNoteForKey(key, Number(fretNumber[1])),
+      index: fretNumber.index,
+    });
+  }
+  return notes;
+};
+
+const getLinesFromFile = path => fs.readFileSync(path, 'utf-8').split('\n');
+
+console.log(
+  getLinesFromFile('./input2.txt')
+    .reduce((notes, line) => notes.concat(extractNotesFromLine(line)), [])
+    .sort((a, b) => a.index - b.index)
+    .map(n => n.name)
+    .join(' ')
+);
