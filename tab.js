366cbb049430d638c80c21d7ca72eef09a7ede57
Daniel Coffman <dgcoffman@gmail.com>
2017-03-06 05:51:20 -0800

    Remove all semicolons to piss off Reddit nerds

--- tab.js
+++ tab.js
@@ -1,24 +1,24 @@
-const fs = require('fs');
+const fs = require('fs')
 
-const FRET_NUMBER_PATTERN = /\d+/g;
-const NOTES = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
+const FRET_NUMBER_PATTERN = /\d+/g
+const NOTES = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
 
 const getNote = (stringTuning, fretNumber) =>
-  NOTES[(NOTES.indexOf(stringTuning) + fretNumber) % NOTES.length];
+  NOTES[(NOTES.indexOf(stringTuning) + fretNumber) % NOTES.length]
 
 const extractNotesFromLine = line => {
-  const notes = [];
-  let fretNumber;
+  const notes = []
+  let fretNumber
   while (fretNumber = FRET_NUMBER_PATTERN.exec(line)) {
     notes.push({
       name: getNote(line.charAt(0), Number(fretNumber[0])),
       pos: fretNumber.index,
-    });
+    })
   }
-  return notes;
-};
+  return notes
+}
 
-const getLinesFromFile = path => fs.readFileSync(path, 'utf-8').split('\n');
+const getLinesFromFile = path => fs.readFileSync(path, 'utf-8').split('\n')
 
 console.log(
   getLinesFromFile('./input2.txt')
@@ -26,4 +26,4 @@ console.log(
     .sort((a, b) => a.pos - b.pos)
     .map(n => n.name)
     .join(' ')
-);
+)
