5d5246761f3f20ff8cca70e61d7da45e93cc75b5
Daniel Coffman <dgcoffman@gmail.com>
2017-03-06 14:41:44 -0800

    Extend to include octaves as per bonus

--- tab.js
+++ tab.js
@@ -3,15 +3,22 @@ const fs = require('fs')
 const FRET_NUMBER_PATTERN = /\d+/g
 const NOTES = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
 
-const getNote = (stringTuning, fretNumber) =>
-  NOTES[(NOTES.indexOf(stringTuning) + fretNumber) % NOTES.length]
+// E4, B3, G3, D3, A2, E2
+// e.g. E4 = (12 * 4) + 4 = 40
+const DEFAULT_TUNING = [40, 35, 31, 26, 21, 16];
 
-const extractNotesFromLine = line => {
+const getNote = (stringTuning, fretNumber) => {
+  const note = NOTES[(stringTuning + fretNumber) % NOTES.length]
+  const octave = Math.floor((stringTuning + fretNumber) / NOTES.length) + 1
+  return `${note}${octave}`
+}
+
+const extractNotesFromLine = (line, index) => {
   const notes = []
   let fretNumber
   while (fretNumber = FRET_NUMBER_PATTERN.exec(line)) {
     notes.push({
-      name: getNote(line.charAt(0), Number(fretNumber[0])),
+      name: getNote(DEFAULT_TUNING[index], Number(fretNumber[0])),
       pos: fretNumber.index,
     })
   }
@@ -21,8 +28,8 @@ const extractNotesFromLine = line => {
 const getLinesFromFile = path => fs.readFileSync(path, 'utf-8').split('\n')
 
 console.log(
-  getLinesFromFile('./input2.txt')
-    .reduce((notes, line) => notes.concat(extractNotesFromLine(line)), [])
+  getLinesFromFile('./bonusInput.txt')
+    .reduce((notes, line, lineIndex) => notes.concat(extractNotesFromLine(line, lineIndex)), [])
     .sort((a, b) => a.pos - b.pos)
     .map(n => n.name)
     .join(' ')
