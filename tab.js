ab07f650adb671670dcea434282456b0b0c2b2ad
Daniel Coffman <dgcoffman@gmail.com>
2017-03-06 05:16:01 -0800

    Try to name things more clearly

--- tab.js
+++ tab.js
@@ -1,18 +1,18 @@
 const fs = require('fs');
 
-const NOTES = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
-const getNoteForKey = (key, fretNumber) =>
-  NOTES[(NOTES.indexOf(key) + fretNumber) % NOTES.length];
-
 const FRET_NUMBER_PATTERN = /\d+/g;
+const NOTES = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
+
+const getNote = (stringTuning, fretNumber) =>
+  NOTES[(NOTES.indexOf(stringTuning) + fretNumber) % NOTES.length];
+
 const extractNotesFromLine = line => {
-  const key = line.charAt(0);
   const notes = [];
   let fretNumber;
   while (fretNumber = FRET_NUMBER_PATTERN.exec(line)) {
     notes.push({
-      name: getNoteForKey(key, Number(fretNumber[0])),
-      index: fretNumber.index,
+      name: getNote(line.charAt(0), Number(fretNumber[0])),
+      pos: fretNumber.index,
     });
   }
   return notes;
@@ -23,7 +23,7 @@ const getLinesFromFile = path => fs.readFileSync(path, 'utf-8').split('\n');
 console.log(
   getLinesFromFile('./input2.txt')
     .reduce((notes, line) => notes.concat(extractNotesFromLine(line)), [])
-    .sort((a, b) => a.index - b.index)
+    .sort((a, b) => a.pos - b.pos)
     .map(n => n.name)
     .join(' ')
 );
